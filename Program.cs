98a1d2f4a6f26ec604840535d10e27c7e7e157f8
thiago3100 <thfelipe31@gmail.com>
2022-07-05 15:07:23 -0300

    exercicio 3

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Globalization;
+namespace Exercicio3
+{
+     public class Exercicio3_udemy
+     {
+        static void Main()
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            int hours = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            float valueHours = float.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            float total = hours * valueHours;
+
+            System.Console.WriteLine("NUMBER = "+ number);
+            Console.WriteLine("SALARY = U$ "+ total.ToString("F2", CultureInfo.InvariantCulture));
+        }
+     }
+}
